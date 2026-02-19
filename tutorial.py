4958b0e4f547e22fbeaff770362a44e834c78093
Amy <amy.platt@hotmail.com>
2026-02-19 20:51:55 +0200

    Add background tiles

--- tutorial.py
+++ tutorial.py
@@ -8,15 +8,33 @@ pygame.init()
 
 pygame.display.set_caption("Platformer") #Sets the name of the game on top of screen
 
-BG_COLOR = (255,255,255)
 WIDTH, HEIGHT = 1000, 800 #Adjust this if youre on a smaller screen
 FPS = 60
 PLAYER_VEL = 5 #Speed in which char moves around the screen
 
 window = pygame.display.set_mode((WIDTH,HEIGHT)) #Creating game window
 
+def get_background(name):
+    image = pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect() #The two _ will pull the information from the image
+    tiles = []
+
+    #Setting x and y axis with the tiles
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
+
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -27,7 +45,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
-            
+        
+        draw(window, background, bg_image)
+
     pygame.quit()
     quit()
 
