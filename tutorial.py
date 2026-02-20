7aaf932de65a5d482c31342aa0687797807817b5
Amy <amy.platt@hotmail.com>
2026-02-20 17:32:45 +0200

    Add blocks, creating the terrain

--- tutorial.py
+++ tutorial.py
@@ -286,7 +286,32 @@ def main(window):
     fire.on()
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
     #Here you can add blocks
-    objects = [*floor, Block(0, HEIGHT - block_size * 2, block_size), Block(block_size * 3, HEIGHT - block_size * 4, block_size), fire]
+    objects = [*floor, 
+               Block(0, HEIGHT - block_size * 2, block_size), 
+               fire,
+               Block(block_size * -8, HEIGHT - block_size * 4, block_size),
+               Block(block_size * -8, HEIGHT - block_size * 3, block_size),
+               Block(block_size * -8, HEIGHT - block_size * 2, block_size),
+               Block(block_size * -8, HEIGHT - block_size * 5, block_size),
+               Block(block_size * 13, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 13, HEIGHT - block_size * 3, block_size),
+               Block(block_size * 13, HEIGHT - block_size * 2, block_size),
+               Block(block_size * 13, HEIGHT - block_size * 5, block_size),
+               Block(block_size * 3, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 4, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 5, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 6, HEIGHT - block_size * 4, block_size),
+               Block(block_size * 8, HEIGHT - block_size * 6, block_size),
+               Block(block_size * 7, HEIGHT - block_size * 6, block_size),
+               Block(block_size * 6, HEIGHT - block_size * 6, block_size),
+               Block(block_size * 5, HEIGHT - block_size * 6, block_size),
+               Block(block_size * -3, HEIGHT - block_size * 4, block_size),
+               Block(block_size * -4, HEIGHT - block_size * 4, block_size),
+               Block(block_size * -5, HEIGHT - block_size * 4, block_size),
+               Block(block_size * -3, HEIGHT - block_size * 6, block_size),
+               Block(block_size * -2, HEIGHT - block_size * 6, block_size),
+               Block(block_size * -1, HEIGHT - block_size * 6, block_size),
+               ]
 
     offset_x = 0
     scroll_area_width = 200
